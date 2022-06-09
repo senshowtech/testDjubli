b81c6257553d84f7c7347e0018842fc02bd078c3
sena <hargiantofebrisena@gmail.com>
2022-06-09 20:39:27 +0700

    initial

--- src/component/FormAddCar.js
+++ src/component/FormAddCar.js
@@ -2,40 +2,63 @@ import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import { useDispatch } from "react-redux";
 import { Add_Car } from "../redux/carSlice";
+import { styled } from "@mui/material/styles";
 
 export default function FormAddCar() {
   const dispatch = useDispatch();
+  const [image, setImage] = React.useState("");
+  const [url, setUrl] = React.useState("");
 
   const HandleSubmit = async (e) => {
     e.preventDefault();
-    let merk = e.target.merk.value;
-    let group_model = e.target.group.value;
-    let model = e.target.model.value;
-    let year = e.target.year.value;
-    let status = e.target.status.value;
-    let km = e.target.km.value;
-    let location = e.target.location.value;
-    let price = e.target.price.value;
-    let data_final = {
-      merk: merk,
-      group_model: group_model,
-      model: model,
-      year: year,
-      status: status,
-      Km: km,
-      location: location,
-      price: price,
-      img: "https://res.cloudinary.com/dumbmerch/image/upload/v1654758971/djubeli/bmw_hf1viv.jpg",
-    };
-    dispatch(Add_Car(data_final));
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "djubli");
+    data.append("cloud_name", "dumbmerch");
+    fetch("https://api.cloudinary.com/v1_1/dumbmerch/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        let merk = e.target.merk.value;
+        let group_model = e.target.group.value;
+        let model = e.target.model.value;
+        let year = e.target.year.value;
+        let status = e.target.status.value;
+        let km = e.target.km.value;
+        let location = e.target.location.value;
+        let price = e.target.price.value;
+        let data_final = {
+          merk: merk,
+          group_model: group_model,
+          model: model,
+          year: year,
+          status: status,
+          Km: km,
+          location: location,
+          price: price,
+          img: data.url,
+        };
+        dispatch(Add_Car(data_final));
+      })
+      .catch((err) => console.log(err));
   };
+
+  const Input = styled("input")({
+    display: "none",
+  });
+
   return (
     <form onSubmit={HandleSubmit}>
       <Box sx={{ display: "flex", flexDirection: "column", mt: 2 }}>
         <Box sx={{ display: "flex", flexDirection: "row" }}>
           <TextField
+            required
             sx={{ width: "50%", mr: 2 }}
             id="outlined-basic"
             label="Merk"
@@ -43,6 +66,7 @@ export default function FormAddCar() {
             variant="outlined"
           />
           <TextField
+            required
             sx={{ width: "50%" }}
             id="outlined-basic"
             label="Group Model"
@@ -52,6 +76,7 @@ export default function FormAddCar() {
         </Box>
         <Box sx={{ display: "flex", flexDirection: "row", mt: 2 }}>
           <TextField
+            required
             sx={{ width: "50%", mr: 2 }}
             id="outlined-basic"
             label="Model"
@@ -59,6 +84,7 @@ export default function FormAddCar() {
             variant="outlined"
           />
           <TextField
+            required
             sx={{ width: "50%" }}
             id="outlined-basic"
             name="year"
@@ -68,6 +94,7 @@ export default function FormAddCar() {
         </Box>
         <Box sx={{ display: "flex", flexDirection: "row", mt: 2 }}>
           <TextField
+            required
             sx={{ width: "50%", mr: 2 }}
             id="outlined-basic"
             name="status"
@@ -75,6 +102,7 @@ export default function FormAddCar() {
             variant="outlined"
           />
           <TextField
+            required
             sx={{ width: "50%" }}
             id="outlined-basic"
             name="km"
@@ -84,6 +112,7 @@ export default function FormAddCar() {
         </Box>
         <Box sx={{ display: "flex", flexDirection: "row", mt: 2 }}>
           <TextField
+            required
             sx={{ width: "50%", mr: 2 }}
             id="outlined-basic"
             name="location"
@@ -91,12 +120,32 @@ export default function FormAddCar() {
             variant="outlined"
           />
           <TextField
+            required
             sx={{ width: "50%" }}
             id="outlined-basic"
             name="price"
             label="Price"
             variant="outlined"
           />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "start", mt: 2 }}>
+          <label htmlFor="icon-button-file">
+            <Input
+              onChange={(e) => setImage(e.target.files[0])}
+              accept="image/*"
+              id="icon-button-file"
+              type="file"
+            />
+            <IconButton
+              sx={{ mr: 2 }}
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <PhotoCamera />
+            </IconButton>
+          </label>
+          <p>{image.name}</p>
         </Box>
         <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
           <Button
