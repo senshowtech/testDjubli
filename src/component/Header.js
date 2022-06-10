77dd7c4e4592b4ee1b48e5fee3e23cdfd277e019
sena <hargiantofebrisena@gmail.com>
2022-06-10 10:51:34 +0700

    initial

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,45 @@
 import React from "react";
-import { ParallaxBanner } from "react-scroll-parallax";
+import Box from "@mui/material/Box";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Box
+      sx={{
+        backgroundRepeat: "no-repeat",
+        backgroundImage: `url(${require("../assets/img/banner.png")})`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 420,
+      }}
+    >
+      <Box
+        sx={{
+          width: "450px",
+        }}
+      >
+        <h2
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: "80px",
+            marginTop: "-60px",
+            textShadow: "0 0 3px black, 0 0 5px black",
+          }}
+        >
+          Djubli
+        </h2>
+        <h4
+          style={{
+            color: "white",
+            textAlign: "center",
+            marginTop: "-60px",
+            textShadow: "0 0 3px black, 0 0 5px black",
+          }}
+        >
+          Temukan Mobil Impianmu
+        </h4>
+      </Box>
+    </Box>
+  );
 }
